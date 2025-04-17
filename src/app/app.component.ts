b3538300d6d0a5f5ce9b6d6d53bb0715d4411c16
Meyioo <tim.meyerhoff@outlook.de>
2025-04-17 21:50:13 +0200

    feat: integrate Tailwind CSS and update project assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { HeaderComponent } from './core/header/header.component';
 import { CryptoApiService } from './services/crypto-api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
